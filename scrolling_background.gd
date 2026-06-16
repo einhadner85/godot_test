6c36a7e041515efdbd6f34854db0a7253f528939
hadong <einhander85@gmail.com>
2026-06-16 16:15:30 +0900

    장애물 씬 추가

--- scrolling_background.gd
+++ scrolling_background.gd
@@ -3,11 +3,11 @@ extends ParallaxBackground
 var scroll_speed = 10.0 # 바닥이 흘러가는 속도
 var move_cnt = 0
 
-func _process(delta):
+func _process(_delta):
 	# 매 프레임마다 배경 전체를 왼쪽(마이너스)으로 이동시켜!
 	# Mirroring을 설정해두었기 때문에 무한히 반복될 거야.
 	move_cnt = move_cnt + scroll_speed
-	scroll_offset.x = scroll_offset.x - move_cnt # (scroll_speed * delta)
+	scroll_offset.x = scroll_offset.x - move_cnt
 
 	# 2. 디버깅용 확인 코드 추가
 	#print("배경 스크립트 실행 중! 현재 위치: ", scroll_offset.x)
